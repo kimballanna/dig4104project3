57d05aa17a576da7bc740a703569b3de52e88aa1
Logan La Vigne <lavigne.logan@knights.ucf.edu>
2015-04-13 09:51:32 -0400

    Major Project Update
    this project now uses a nice version of angular js. in order to work
    with it you need to have the folder in htdocs and run mamp/wamp as a
    localhost:8888.
    
    fictitious: the previous app committed suicide.

---
+++ japan/js/main.js
@@ -0,0 +1,52 @@
+
+/**
+ * Main AngularJS Web Application
+ */
+var app = angular.module('tutorialWebApp', [
+  'ngRoute'
+]);
+
+/**
+ * Configure the Routes
+ */
+app.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+    // Home
+    .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
+    // Pages
+    .when("/prep", {templateUrl: "partials/prep.html", controller: "PageCtrl"})
+    .when("/food", {templateUrl: "partials/food.html", controller: "PageCtrl"})
+    .when("/culture", {templateUrl: "partials/culture.html", controller: "PageCtrl"})
+    .when("/language", {templateUrl: "partials/language.html", controller: "PageCtrl"})
+    .when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
+    .when("/tourist", {templateUrl: "partials/tourist.html", controller: "PageCtrl"})
+    // Blog
+    .when("/blog", {templateUrl: "partials/blog.html", controller: "BlogCtrl"})
+    .when("/blog/post", {templateUrl: "partials/blog_item.html", controller: "BlogCtrl"})
+    // else 404
+    .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
+}]);
+
+/**
+ * Controls the Blog
+ */
+app.controller('BlogCtrl', function (/* $scope, $location, $http */) {
+  console.log("Blog Controller reporting for duty.");
+});
+
+/**
+ * Controls all other Pages
+ */
+app.controller('PageCtrl', function (/* $scope, $location, $http */) {
+  console.log("Page Controller reporting for duty.");
+
+  // Activates the Carousel
+  $('.carousel').carousel({
+    interval: 5000
+  });
+
+  // Activates Tooltips for Social Links
+  $('.tooltip-social').tooltip({
+    selector: "a[data-toggle=tooltip]"
+  })
+});
